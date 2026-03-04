22bc53082a378f2ce8878999a2b56a7cc4fe65dd
Alix <alixdet@protonmail.com>
2026-03-04 11:13:30 +0100

    docs: improve CsvIO docstrings and add type information

--- CsvIO.py
+++ CsvIO.py
@@ -27,20 +27,45 @@ from . import Subject
 
 class CsvIO():
     """
-    This class takes input csv filehandle and returns contigs that
-    matched in blast search.
-    It returns "null" in case contig did not match or end of file.
+    Parse BLAST CSV output and return contigs with their subject matches.
+
+    This class reads BLAST CSV output line by line and groups output by contig ID,
+    creating Contig objects and associating Subject objects with each match.
+
+    Attributes:
+        handle: File handle to BLAST CSV output
+        minDec: Minimum number of hits required (filter parameter)
+        minBpLength: Minimum alignment length required (filter parameter)
+        program: BLAST program type ('blastn' or 'blastx')
     """
 
     def __init__(self, handle, minDec, minBpLength, program):
+        """
+        Initialize a BLAST CSV output parser.
+
+        Args:
+            handle: File handle to BLAST CSV output
+            minDec: Minimum number of hits to consider (int)
+            minBpLength: Minimum alignment length in base pairs (int)
+            program: BLAST program type ('blastn' or 'blastx')
+        """
         self.handle = handle
         self.minDec = int(minDec)
         self.minBpLength = int(minBpLength)
         self.program = program
 
     def next(self, s_id):
-        """For a given contig search its id in BLAST csv output
-        Load it with subject class if exists... else return null
+        """
+        Get next contig from BLAST output and its matches.
+
+        Reads lines from BLAST CSV output until all matches for a given contig ID
+        are collected, then returns a Contig object with all its Subject matches.
+
+        Args:
+            s_id: Contig ID to search for
+
+        Returns:
+            Contig object with matches if found, None if contig not in output
         """
         pos = self.handle.tell()
         bl = self.handle.readline().split()
@@ -52,12 +77,12 @@ class CsvIO():
         c = Contig.Contig(bl[0], self.minDec, self.minBpLength)  # init Contig
         # if Current contig (from fasta), does not appear in (output) csv file
         if c.id != s_id:
-            self.handle.seek(pos)  # retour a position initiale
+            self.handle.seek(pos)  # Return to initial position
             return None
 
         c.subject.append(Subject.Subject(bl, self.program))
 
-        # takes subsequent lines with the same query contig id
+        # Collect subsequent lines with the same query contig id
         while c.id == s_id:
             pos = self.handle.tell()
             bl = self.handle.readline().split()
@@ -66,7 +91,7 @@ class CsvIO():
             c.id = bl[0]
             c.subject.append(Subject.Subject(bl, self.program))
 
-#       else:  # end of this contig
+        # End of this contig's matches
         c.id = s_id
         c.subject = c.subject[:-1]
         self.handle.seek(pos)
