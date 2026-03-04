27e58ec8acffab1b186c7c8c6e89c85f33cb6d4c
Alix <alixdet@protonmail.com>
2026-03-04 10:37:08 +0100

    feat: add package __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""
+d-chimer: Detect and assign chimeric sequences in taxonomic assignment with BLAST
+
+This package detects and assigns fragments of contigs included in chimeric sequences
+during taxonomic assignment with BLAST. It supports both BLASTn and BLASTx workflows.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Sourakhata Tirera, Jean-Marc Frigerio, Alix de Thoisy"
+__author_email__ = "alixdet@protonmail.com"
+__license__ = "GNU General Public License v3.0"
